2d57f9e1fb50c4ff218e7179dfcb7261b0a0d815
Gilberto Rosario <gilberto.rosario@gilberto.rosario>
2023-09-25 12:10:00 -0400

    added code for server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const session = require("express-session");
+const sequelize = require("./config/connection");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(
+  session({
+    secret: "your-secret-key",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
+});
