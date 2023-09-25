3be72fbbe9848b573520f3247f6421ed4acd6db5
Gilberto Rosario <gilberto.rosario@gilberto.rosario>
2023-09-25 12:27:41 -0400

    js code for login, logout, user signup

---
+++ routes/api/auth.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const router = express.Router();
+const { User } = require("../../models");
+
+// User signup
+router.post("/signup", async (req, res) => {
+  try {
+    const userData = await User.create(req.body);
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+      res.status(200).json(userData);
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// User login
+router.post("/login", async (req, res) => {
+  try {
+    const userData = await User.findOne({ where: { email: req.body.email } });
+    if (!userData || !userData.checkPassword(req.body.password)) {
+      res.status(401).json({ message: "Invalid credentials" });
+      return;
+    }
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+      res.status(200).json({ user: userData, message: "You are now logged in!" });
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// User logout
+router.post("/logout", (req, res) => {
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
+module.exports = router;
