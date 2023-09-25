0f76988007038a32dc9dbde3a23db289e631b647
Gilberto Rosario <gilberto.rosario@gilberto.rosario>
2023-09-25 12:30:47 -0400

    added some js code for the auth controller

---
+++ controllers/authController.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const router = express.Router();
+const { User } = require("../models");
+const bcrypt = require("bcrypt");
+
+// User signup
+router.post("/signup", async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+    const userData = await User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+      res.status(200).json(userData);
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// User login
+router.post("/login", async (req, res) => {
+  try {
+    const userData = await User.findOne({ where: { email: req.body.email } });
+    if (!userData || !(await bcrypt.compare(req.body.password, userData.password))) {
+      res.status(401).json({ message: "Invalid credentials" });
+      return;
+    }
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+      res.status(200).json({ user: userData, message: "You are now logged in!" });
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// User logout
+router.post("/logout", (req, res) => {
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
+module.exports = router;
