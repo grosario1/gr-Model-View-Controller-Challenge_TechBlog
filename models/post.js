4f4d86ebfc76e89d721f775bd2e2053ad8eb4591
Gil Rosario <grosario@alum.quinnipiac.edu>
2023-09-26 19:20:31 -0400

    update to models

--- models/post.js
+++ models/post.js
@@ -1,9 +1,9 @@
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
 
-class Post extends Model {}
+class Blog extends Model {}
 
-Post.init(
+Blog.init(
   {
     id: {
       type: DataTypes.INTEGER,
