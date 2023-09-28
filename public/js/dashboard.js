dae18aaa48f68c8cdfb09e5b8239c11c984ac940
Gil Rosario <grosario@alum.quinnipiac.edu>
2023-09-27 22:10:18 -0400

    restored files

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,40 +1,39 @@
 document.getElementById("create-post-form").addEventListener("submit", (event) => {
-    event.preventDefault();
-  
-    const title = document.getElementById("post-title").value.trim();
-    const content = document.getElementById("post-content").value.trim();
-  
-    // create a new post
-    fetch("/api/posts", {
-      method: "POST",
-      body: JSON.stringify({ title, content }),
-      headers: {
-        "Content-Type": "application/json",
-      },
+  event.preventDefault();
+
+  const title = document.getElementById("post-title").value.trim();
+  const content = document.getElementById("post-content").value.trim();
+
+  // create a new post
+  fetch("/api/posts", {
+    method: "POST",
+    body: JSON.stringify({ title, content }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then(() => {
+      window.location.replace("/dashboard");
+    })
+    .catch((error) => {
+      console.error("Error creating post:", error);
+    });
+});
+
+const deleteButtons = document.querySelectorAll(".delete-post-btn");
+deleteButtons.forEach((button) => {
+  button.addEventListener("click", function () {
+    const postId = this.getAttribute("data-post-id");
+
+    // delete the post
+    fetch(`/api/blogs/${blogId}`, {
+      method: "DELETE",
     })
       .then(() => {
-        window.location.replace("/dashboard");
+        location.reload();
       })
       .catch((error) => {
-        console.error("Error creating post:", error);
+        console.error("Error deleting post:", error);
       });
   });
-  
-  const deleteButtons = document.querySelectorAll(".delete-post-btn");
-  deleteButtons.forEach((button) => {
-    button.addEventListener("click", function () {
-      const postId = this.getAttribute("data-post-id");
-  
-      // delete the post
-      fetch(`/api/blogs/${blogId}`, {
-        method: "DELETE",
-      })
-        .then(() => {
-          location.reload();
-        })
-        .catch((error) => {
-          console.error("Error deleting post:", error);
-        });
-    });
-  });
-  
+});
--- public/js/login.js
+++ public/js/login.js
@@ -20,5 +20,4 @@ async function loginFormHandler(event) {
     }
   }
 };
-
   
