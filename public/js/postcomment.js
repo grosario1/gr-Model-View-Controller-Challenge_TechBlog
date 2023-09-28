32af6e36f96d2ed41fd9dc7e8a984f9433a04888
Gil Rosario <grosario@alum.quinnipiac.edu>
2023-09-27 21:34:26 -0400

    removed login link from homepage hbs

--- public/js/postcomment.js
+++ public/js/postcomment.js
@@ -1,21 +1,26 @@
 
-document.getElementById("comment-form").addEventListener("submit", (event) => {
-    event.preventDefault();
-  
-    const text = document.getElementById("comment-text").value.trim();
-  
-    //create a new comment
-    fetch(`/api/comments/${blogId}`, {
-      method: "POST",
-      body: JSON.stringify({ text }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then(() => {
-        location.reload();
-      })
-      .catch((error) => {
-        console.error("Error submitting comment:", error);
-      });
-  });  
+const commentFormHandler = async function (event) {
+	event.preventDefault();
+
+	const blog_id = document.querySelector('.new-comment-form').dataset.blogid;
+
+	const comment_description = document.querySelector('#comment-text').value.trim();
+
+	if (comment_description) {
+		await fetch('/api/comments', {
+			method: 'POST',
+			body: JSON.stringify({
+				blog_id,
+				comment_description,
+			}),
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		});
+		document.location.reload();
+	}
+};
+
+document
+	.querySelector('.new-comment-form')
+	.addEventListener('submit', commentFormHandler);
